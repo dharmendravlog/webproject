522a689fb4fc2762178cd066391b5a15f5430a96
DHARMENDRA SINGH THAKUR <dharmendra.developers@gmail.com>
2023-07-08 14:21:00 +0530

    adding back button

--- src/app.js
+++ src/app.js
@@ -52,9 +52,9 @@ const port = process.env.PORT || 8000;
 
 //public static path
 //console.log(path.join(__dirname,"../public"));
-const static_path = path.join(__dirname,"../public");
-const template_path = path.join(__dirname,"../templates/views");
-const partials_path = path.join(__dirname,"../templates/partials");
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+const partials_path = path.join(__dirname, "../templates/partials");
 
 app.set("view engine", "hbs");
 app.set('views', template_path);
@@ -76,17 +76,66 @@ app.get("/weather", (req, res) => {
 });
 app.get("*", (req, res) => {
     res.render("404error", {
-        errorMsg : "Opps! Page Not Found"
+        errorMsg: "Opps! Page Not Found , Click Here to go"
     });
 });
 
 app.listen(port, () => {
-console.log(`listening to the port at ${port}`);
+    console.log(`listening to the port at ${port}`);
 });
 
 //#14
-/*
+/* by cmd ya gitbase
  SSH secure shell or ssh is used to create a secure channel between a local and remote computer.while ssh is commonly used for secure terminal access and
  file tranfers, it can also be used to create a secure tunnel between computers for forwarding other network connections that are not normally encrypted 
+expressWeb>git status              //initialize hai ki nhi check           fatal error aaye to git init
+          >git init
+          >git status                                            // red dikhege
+          >git add .                                             // for single git add public  
+          >git commit -m "nodejs project first commit"
+          >git status                                            // on branch master
 
-*/
+          github
+          create new repo   webproject
+          git remote add origin https://github.com/dharmendravlog/webproject.git
+
+          >git remote                                            //none
+          >git remote add origin https://github.com/dharmendravlog/webproject.git
+          >git remote                                            //origin
+
+
+          for ssh key use gitbase
+          >ls ~/.ssh                                            // error
+
+          >ssh-keygen -t rsa -b 4096 -C "dharmendra.developers@gmail.com"
+          //path           enter
+          //password       enter enter
+          >ctrl+c
+          >ls ~/.ssh       id_rsa id_rsa.pub 
+
+          private key rsa ko sys me add krne ke liye agent chahiye ssh ka live hona chahiye
+          
+          >eval ${ssh-agent -s}        Agent pid 1394  //make sure live hoga to agent pid aaygi milegi
+          
+          >ssh-add -K /Users/Dell/.ssh/id_rsa or ssh-add -K ~/.ssh/id_rsa
+
+          >cat ~/.ssh/id_rsa.pub    
+          
+          jo bhi code aaye use copy krke github me profilesetting me ssh key me new key generate krke title name de and key me code paste kare
+
+          sys or github me proper secure connection estabilish ho gya hai ki nhi check
+
+          in cmd
+          >ssh -T git@github.com                                   //  first bar yes            
+
+          >git remote                                              //origin
+          >git branch                                              * master 
+          >git push -u origin master
+
+          //in future hmne apne project me kuch kuch edit kiya to 
+
+          >git status                                     file red
+          >git add.
+          >git status                                     file grren
+          >git commit -m "adding back button"
+          */
